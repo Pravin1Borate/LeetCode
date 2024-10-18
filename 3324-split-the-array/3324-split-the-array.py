df757d0ee33537ce2401b9d98bbce6a3e0d6cc1f
Pravin Borate <49568861+Pravin1Borate@users.noreply.github.com>
2024-10-18 16:14:47 +0530

    Time: 0 ms (100%), Space: 16.7 MB (13.06%) - LeetHub

---
+++ 3324-split-the-array/3324-split-the-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPossibleToSplit(self, nums: List[int]) -> bool:
+        counter = Counter(nums)
+
+        for val in counter.values():
+            if val > 2:
+                return False
+        return True
+        
